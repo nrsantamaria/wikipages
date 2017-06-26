4f0e7fbf6cdb8495d265fc26a623df5ad403687d
James Higgins and Nicole Santamaria <james@>
2017-06-26 11:00:34 -0700

    add initial files and create types model.

---
+++ app/controllers/types_controller.rb
@@ -0,0 +1,25 @@
+class TypesController < ApplicationController
+  def index
+    @types = Type.all
+    render :index
+  end
+
+  def new
+    @type = Type.new
+    render :new
+  end
+
+  def create
+    @type = Type.new(type_params)
+    if @type.save
+      redirect_to types_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def type_params
+    params.require(:type).permit(:name)
+  end
+end
