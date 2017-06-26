d70cf50627e0580e8eef889abb0db14056d541f1
James Higgins and Nicole Santamaria <nicole@>
2017-06-26 13:24:30 -0700

    add edit and update for businesses

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -3,6 +3,7 @@ class BusinessesController < ApplicationController
   def new
     @type = Type.find(params[:type_id])
     @business = @type.businesses.new
+    render :new
   end
 
   def create
@@ -12,6 +13,22 @@ class BusinessesController < ApplicationController
       redirect_to type_path(@business.type)
     else
       render :new
+    end
+  end
+
+  def edit
+    @type = Type.find(params[:type_id])
+    @business = Business.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @type = Type.find(params[:type_id])
+    @business = Business.find(params[:id])
+    if @business.update(business_params)
+      redirect_to type_path(@business.type)
+    else
+      render :edit
     end
   end
 
